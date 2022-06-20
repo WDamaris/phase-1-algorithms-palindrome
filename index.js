7d79336cf10004ce585233b99d0139ae5b3e1f2f
WDamaris <damarisshish@gmail.com>
2022-06-20 08:48:12 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+    // Write your algorithm here
+    let wordLowerCase=word.toLowerCase();
+    let wordSeparate=wordLowerCase.split('').reverse().join('')
+    if(wordLowerCase===wordSeparate){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
+  /* 
+    Add your pseudocode here
+  */
+  
+  /*
+    Add written explanation of your solution here
+  */
+  
+  // You can run `node index.js` to view these console logs
+  if (require.main === module) {
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+    console.log("");
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
+  }
+  module.exports = isPalindrome; // Write your algorithm here
+
 
 /* 
   Add your pseudocode here
